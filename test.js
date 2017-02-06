3fcd66ce5e704097017b5f17236ee88c5a95183c
Forrest Desjardins <desjardinsfg@gmail.com>
2017-02-05 17:37:50 -0900

    Ensure procedures are received in tests

--- test.js
+++ test.js
@@ -1,21 +1,30 @@
 /* global describe, it */
 
+const assert = require('chai').assert
 const terminalProcedures = require('./index')
 
 describe('terminalProcedures', () => {
-  it('should fetch terminal procedures for a single ICAO', (done) => {
-    terminalProcedures('KFDC').then(() => done())
+  it('should fetch terminal procedures for a single ICAO', () => {
+    return terminalProcedures('PANC').then(procedures => {
+      assert(procedures)
+    })
   })
 
-  it('should fetch terminal procedures for an array of ICAOs', (done) => {
-    terminalProcedures(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch terminal procedures for an array of ICAOs', () => {
+    return terminalProcedures(['PANC', 'KSFO']).then(procedures => {
+      assert(procedures.length === 2)
+      procedures.map(assert)
+    })
   })
 
-  it('should expose the list method', (done) => {
-    terminalProcedures.list('KFDC').then(() => done())
+  it('should expose the list method', () => {
+    return terminalProcedures.list('PANC')
   })
 
-  it('should fetch terminal procedures for an array of ICAOs using the list method', (done) => {
-    terminalProcedures.list(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch terminal procedures for an array of ICAOs using the list method', () => {
+    return terminalProcedures.list(['PANC', 'KSFO']).then(procedures => {
+      assert(procedures.length === 2)
+      procedures.map(assert)
+    })
   })
 })
